d1883f089afc488e554810b9de41e58a9bcbcbdc
Richard Février <richard@fevrier.io>
2023-10-11 18:16:34 +0200

    Wezterm update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,16 +6,8 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.color_scheme = 'Bogster'
 config.front_end = "WebGpu"
-
-config.window_background_opacity = 0
-config.macos_window_background_blur = 40
-
 config.enable_scroll_bar = true
-
-config.color_scheme = 'Dracula'
-config.colors = {
-  scrollbar_thumb = '#f8f8f2'
-}
 
 return config
