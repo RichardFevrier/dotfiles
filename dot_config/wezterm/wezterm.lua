2e330b47919d19ffffb675358fca48ed60589ab4
Richard Février <richard@fevrier.io>
2024-05-06 22:55:54 +0200

    Wezterm update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,8 +6,9 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Bogster'
+config.color_scheme = 'rose-pine'
 config.front_end = 'WebGpu'
 config.enable_scroll_bar = true
+config.scrollback_lines = 100000
 
 return config
