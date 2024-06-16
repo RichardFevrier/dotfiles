5bfc848f574cc39de4e010b8be66fea2538754b6
Richard Février <richard@fevrier.io>
2024-06-16 19:42:25 +0200

    Project cleanup

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,10 +7,15 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = 'dobri_next_a00_black'
-config.font = wezterm.font('Meslo LG M')
-config.font_size = 12.0
+config.font = wezterm.font {
+  family = 'Fira Code',
+  harfbuzz_features = { 'cv02', 'cv29', 'ss01', 'ss03', 'ss05' },
+}
+config.font_size = 14.5
+config.line_height = 1.22
 config.front_end = 'WebGpu'
 config.enable_scroll_bar = true
 config.scrollback_lines = 100000
+config.check_for_updates = false
 
 return config
