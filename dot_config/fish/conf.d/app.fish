91776e1f22d9371eca7e8750fc3c5412b681eeea
Richard Février <richard@fevrier.io>
2025-06-25 12:12:40 +0200

    App update

--- dot_config/fish/conf.d/app.fish
+++ dot_config/fish/conf.d/app.fish
@@ -1,5 +1,28 @@
 if status is-interactive
     if type -q app
-        abbr -a app_brew_diff "set --local app_output (cat ~/.config/app/packages/brew.json); for i in (brew leaves); if not string match -rq \$i \$app_output; echo \$i; end; end"
+        function app_brew_diff
+            app_diff ~/.config/app/packages/brew.json "brew leaves"
+        end
+
+        function app_flatpak_diff
+            app_diff ~/.config/app/packages/flatpak.json "flatpak list --app --columns=application"
+        end
+
+        function app_diff --argument-names json_path system_cmd
+            set --local app_output (string split ' ' (string match -r '"packages": "(.*?)"' (cat $json_path))[2])
+            set --local system_output (eval $system_cmd)
+
+            for app in $app_output
+                if not contains -- $app $system_output
+                    echo "Missing from system: $app"
+                end
+            end
+
+            for app in $system_output
+                if not contains -- $app $app_output
+                    echo "Not listed in file: $app"
+                end
+            end
+        end
     end
 end
